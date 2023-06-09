41ed53003208de3bad82c29a87aad091b9dd3242
Vanya_Coffee <vanakupnevskij@gmail.com>
2023-06-09 20:53:54 +0300

    auth

--- src/modules/AuthorizationModule/components/ModuleMain/index.jsx
+++ src/modules/AuthorizationModule/components/ModuleMain/index.jsx
@@ -3,22 +3,29 @@ import styles from './style.module.scss';
 
 import Button from '../../../../ui/Button';
 import TextInput from '../../../../ui/TextInput';
+import SelectInput from '../../../../ui/SelectInput';
 import { useHttp } from '../../../../hooks/http.hook';
 import { useState } from 'react';
 import { useAuth } from '../../../../hooks/auth.hook';
 
 function AuthorizationModule() {
   const classNameRoot = `${panelGlobalStyle.panel} ${styles.root} `;
-  const { login, isAuthorization, name, email, role, name_subject } = useAuth();
+  const { login, isAuthorization, name, email, token, role, name_subject } = useAuth();
   const { loading, request, error, clearError } = useHttp();
   const [form, setForm] = useState({
     email: '',
     password: '',
     name: '',
-    role: '',
+    role: 'student',
     name_subject: '',
   });
   const [isLogin, setIsLogin] = useState(true);
+
+  const listRoles = [
+    { key: 'student', value: 'Учень' },
+    { key: 'teacher', value: 'Вчитель' },
+    { key: 'admin', value: 'Адмін' },
+  ];
 
   async function handleClickSubmit() {
     try {
@@ -41,12 +48,13 @@ function AuthorizationModule() {
             role: form.role,
             name_subject: form.name_subject,
           },
+          bearerToken: token,
         });
 
         setIsLogin(true);
       }
 
-      setForm({ email: '', password: '', name: '', role: '', name_subject: '' });
+      setForm({ email: '', password: '', name: '', role: 'student', name_subject: '' });
     } catch (e) {}
   }
 
@@ -54,16 +62,18 @@ function AuthorizationModule() {
     clearError();
 
     setForm({ ...form, [name]: newValue });
+
+    console.log({ ...form, [name]: newValue });
   }
 
   function handleChangeType() {
     setIsLogin((prev) => !prev);
-    setForm({ email: '', password: '', name: '', role: '', name_subject: '' });
+    setForm({ email: '', password: '', name: '', role: 'student', name_subject: '' });
   }
 
   return (
     <>
-      {isAuthorization ? (
+      {isAuthorization && (
         <div className={classNameRoot}>
           <h6 className={styles.title}>Дані користувача</h6>
           <dl className={styles.list_info}>
@@ -87,65 +97,66 @@ function AuthorizationModule() {
             )}
           </dl>
         </div>
-      ) : (
-        <div className={classNameRoot}>
-          <h6 className={styles.title}>{isLogin ? 'Вхід' : 'Реєстрація'}</h6>
-          {error && <p className={styles.error_message}>{error.message}</p>}
-          {!isLogin && (
-            <TextInput
-              name={'name'}
-              placeholder={"Уведіть ім'я..."}
-              label={"Ім'я"}
-              value={form.name}
-              onChange={(value) => handleChange('name', value)}
+      )}
+      <div className={classNameRoot}>
+        <h6 className={styles.title}>{isLogin ? 'Вхід' : 'Реєстрація'}</h6>
+        {error && <p className={styles.error_message}>{error.message}</p>}
+        {!isLogin && (
+          <TextInput
+            name={'name'}
+            placeholder={"Уведіть ім'я..."}
+            label={"Ім'я"}
+            value={form.name}
+            onChange={(value) => handleChange('name', value)}
+            onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
+          />
+        )}
+        <TextInput
+          name={'email'}
+          placeholder={'Уведіть електронну пошту...'}
+          label={'Email'}
+          value={form.email}
+          onChange={(value) => handleChange('email', value)}
+          onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
+        />
+        <TextInput
+          name={'password'}
+          type="password"
+          placeholder={'Уведіть пароль...'}
+          label={'Пароль'}
+          value={form.password}
+          onChange={(value) => handleChange('password', value)}
+          onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
+        />
+        {!isLogin && (
+          <>
+            <SelectInput
+              className={styles.select_role}
+              name={'role'}
+              placeholder={'Уведіть вашу роль...'}
+              label={'Роль'}
+              options={listRoles}
+              value={form.role}
+              onChange={(value) => handleChange('role', value)}
               onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
             />
-          )}
-          <TextInput
-            name={'email'}
-            placeholder={'Уведіть електронну пошту...'}
-            label={'Email'}
-            value={form.email}
-            onChange={(value) => handleChange('email', value)}
-            onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
-          />
-          <TextInput
-            name={'password'}
-            type="password"
-            placeholder={'Уведіть пароль...'}
-            label={'Пароль'}
-            value={form.password}
-            onChange={(value) => handleChange('password', value)}
-            onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
-          />
-          {!isLogin && (
-            <>
-              <TextInput
-                name={'role'}
-                placeholder={'Уведіть вашу роль...'}
-                label={'Роль'}
-                value={form.role}
-                onChange={(value) => handleChange('role', value)}
-                onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
-              />
-              <TextInput
-                name={'name_subject'}
-                placeholder={'Уведіть назву предмету...'}
-                label={'Назва предмету'}
-                value={form.name_subject}
-                onChange={(value) => handleChange('name_subject', value)}
-                onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
-              />
-            </>
-          )}
-          <Button className={styles.submit_button} onClick={handleClickSubmit} disabled={loading}>
-            {loading ? 'Завантажуємо...' : isLogin ? 'Увійти' : 'Зареєструватися'}
-          </Button>
-          <p className={styles.another_link} onClick={handleChangeType}>
-            {isLogin ? 'реєстрація' : 'вхід'}
-          </p>
-        </div>
-      )}
+            <TextInput
+              name={'name_subject'}
+              placeholder={'Уведіть назву предмету...'}
+              label={'Назва предмету'}
+              value={form.name_subject}
+              onChange={(value) => handleChange('name_subject', value)}
+              onKeyUp={(e) => e.key === 'Enter' && handleClickSubmit()}
+            />
+          </>
+        )}
+        <Button className={styles.submit_button} onClick={handleClickSubmit} disabled={loading}>
+          {loading ? 'Завантажуємо...' : isLogin ? 'Увійти' : 'Зареєструватися'}
+        </Button>
+        <p className={styles.another_link} onClick={handleChangeType}>
+          {isLogin ? 'реєстрація' : 'вхід'}
+        </p>
+      </div>
     </>
   );
 }
